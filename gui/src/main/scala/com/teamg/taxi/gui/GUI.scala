3948a966f34e125bc910474e7eadcfbaf5aa9cd7
olabudzyn <ola.budz96@gmail.com>
2020-06-14 20:58:29 +0200

    created animation with changed taxi state and location (#24)

--- gui/src/main/scala/com/teamg/taxi/gui/GUI.scala
+++ gui/src/main/scala/com/teamg/taxi/gui/GUI.scala
@@ -1,18 +1,24 @@
 package com.teamg.taxi.gui
 
+import com.teamg.taxi.core.api.{Location, Taxi, TaxiState}
 import com.teamg.taxi.core.map.MapProvider
+import scalafx.animation.AnimationTimer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Point2D
+import scalafx.scene.paint.Color
 import scalafx.scene.paint.Color._
-import scalafx.scene.shape._
+import scalafx.scene.shape.{Circle, LineTo, MoveTo, Path, PathElement}
 import scalafx.scene.{Group, Scene}
+
+import scala.util.Random
 
 object GUI extends JFXApp {
 
   val pointSize = 10
   val boardWidth = 800
   val boardHeight = 800
+  val refreshTime = 1000000000L
   private val cityMap = MapProvider.default
 
   stage = new PrimaryStage {
@@ -21,15 +27,72 @@ object GUI extends JFXApp {
     height = boardHeight
     scene = new Scene {
       fill = WhiteSmoke
-      val canvas = new Group()
+      val canvas = new Group
+      createStaticMap()
 
-      val circles: List[Circle] = cityMap.getCityMapElements.nodes.map(node =>  createCircle(node.location.x, node.location.y)).toList
-      val paths: List[Path] = cityMap.getCityMapElements.edges.map(edge => generatePath(List(new Point2D(edge.first.location.x, edge.first.location.y),
-        new Point2D(edge.second.location.x, edge.second.location.y))))
+      val taxis = List(Taxi("Taxi1", Location(Random.nextInt(boardWidth - 50), Random.nextInt(boardHeight - 50)), TaxiState.Free),
+        Taxi("Taxi2", Location(Random.nextInt(boardWidth - 50), Random.nextInt(boardHeight - 50)), TaxiState.Free),
+        Taxi("Taxi3", Location(Random.nextInt(boardWidth - 50), Random.nextInt(boardHeight - 50)), TaxiState.Free)
+      )
 
-      circles.foreach(circle => canvas.getChildren.add(circle))
-      paths.foreach(path => canvas.getChildren.add(path))
+      val taxiCircles: List[Circle] = taxis.map(t => drawTaxi(t))
+      taxiCircles.foreach(tc => canvas.getChildren.add(tc))
+
       content = canvas
+
+      var lastUpdate = 0L
+      val timer: AnimationTimer = AnimationTimer { now => {
+        if (now - lastUpdate >= refreshTime) {
+          taxiCircles.foreach(tc => canvas.getChildren.remove(tc))
+          taxiCircles.foreach(tc => tc.fill = checkState(randomState()))
+          taxiCircles.foreach(tc => {
+            val newLocation = getRandomLocation()
+            tc.relocate(newLocation.x, newLocation.y)
+          })
+          taxiCircles.foreach(tc => canvas.getChildren.add(tc))
+          lastUpdate = now
+        }
+      }
+      }
+
+      timer.start()
+
+      def createStaticMap(): Unit = {
+        val circles: List[Circle] = cityMap.getCityMapElements.nodes.map(node => createCircle(node.location.x, node.location.y))
+        val paths: List[Path] = cityMap.getCityMapElements.edges.map(edge => generatePath(List(new Point2D(edge.first.location.x, edge.first.location.y),
+          new Point2D(edge.second.location.x, edge.second.location.y))))
+        circles.foreach(circle => canvas.getChildren.add(circle))
+        paths.foreach(path => canvas.getChildren.add(path))
+      }
+
+      def randomState(): TaxiState = {
+        val number = Random.nextInt(3)
+        number match {
+          case 0 => TaxiState.Free
+          case 1 => TaxiState.Occupied
+          case 2 => TaxiState.OnWayToCustomer
+        }
+      }
+
+      def getRandomLocation(): Location = {
+        Location(Random.nextInt(boardWidth-50), Random.nextInt(boardHeight-50))
+      }
+
+      def drawTaxi(taxi: Taxi): Circle = {
+        val circle = new Circle()
+        circle.fill = checkState(taxi.taxiState)
+        circle.radius = pointSize
+        circle.relocate(taxi.location.x, taxi.location.y)
+        circle
+      }
+
+      def checkState(state: TaxiState): Color = {
+        state match {
+          case TaxiState.Free => Green
+          case TaxiState.OnWayToCustomer => DarkMagenta
+          case TaxiState.Occupied => Red
+        }
+      }
 
       def createCircle(x: Double, y: Double): Circle = {
         val circle = new Circle()
