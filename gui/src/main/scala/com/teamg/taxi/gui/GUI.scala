75cc153cf2d35d9bf22defc82b253be1370aedac
olabudzyn <ola.budz96@gmail.com>
2020-06-15 21:06:58 +0200

    added getting taxis and update localization from api (#35)

--- gui/src/main/scala/com/teamg/taxi/gui/GUI.scala
+++ gui/src/main/scala/com/teamg/taxi/gui/GUI.scala
@@ -17,12 +17,13 @@ import scalafx.scene.{Group, Scene}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
-import scala.util.{Failure, Random, Success}
+import scala.runtime.Tuple2Zipped
+import scala.util.Random
 
 class GUI(taxiSystem: TaxiSystem,
           config: SimulationConfig)
          (implicit executionContext: ExecutionContext) extends JFXApp {
-  
+
   stage = new PrimaryStage() {
     title = "Taxi System"
     width = boardWidth
@@ -32,87 +33,91 @@ class GUI(taxiSystem: TaxiSystem,
 
   private def createScene(): Scene = {
     new Scene {
-      fill = WhiteSmoke
+      fill = boardColor
       val canvas = new Group
 
       createStaticMap(canvas)
       createLegend(canvas)
 
-      Thread.sleep(5000)
-      val response = taxiSystem.receive
-
-      val responseTaxiState: TaxiSystemState = Await.result(response, Duration(2, TimeUnit.SECONDS))
-
-      val responseTaxis = responseTaxiState.taxis
-
-      //      println(responseTaxis)
-      //      val taxis = List(Taxi("Taxi1", Location(Random.nextInt(boardWidth - 50), Random.nextInt(boardHeight - 50)), TaxiState.Free),
-      //        Taxi("Taxi2", Location(Random.nextInt(boardWidth - 50), Random.nextInt(boardHeight - 50)), TaxiState.Free),
-      //        Taxi("Taxi3", Location(Random.nextInt(boardWidth - 50), Random.nextInt(boardHeight - 50)), TaxiState.Free)
-      //      )
-
-      val taxiLabels: List[Text] = responseTaxis.map(t => createText(t.id, t.location))
-
-      val taxiCircles: List[Circle] = responseTaxis.map(t => drawTaxi(t))
-      taxiCircles.foreach(tc => canvas.getChildren.add(tc))
-      taxiLabels.foreach(label => canvas.getChildren.add(label))
-
-      val taxiWithLabel = (taxiLabels, taxiCircles).zipped
-
-
-      //      var responseTaxi: List[Taxi]
-
-      response.andThen({
-        case Success(response) => response
-        case Failure(_) => None
-      })
+      var taxiWithLabel = initializeView(canvas, config)
 
       content = canvas
 
-      var lastUpdate = 0L
       val timer: AnimationTimer = AnimationTimer { now => {
         if (now - lastUpdate >= refreshTime) {
-          taxiWithLabel.foreach((label, taxi) => canvas.children.removeAll(label, taxi))
-          taxiWithLabel.foreach((label, taxi) => {
-            taxi.fill = checkState(randomState())
-            val newLocation = getRandomLocation
-            taxi.relocate(newLocation.x, newLocation.y)
-            label.relocate(newLocation.x + padding, newLocation.y + padding)
-
-          })
-          taxiWithLabel.foreach((label, taxi) => canvas.children.addAll(label, taxi))
+          val future = taxiSystem.receive.map(r => updateValues(r))
+          val responseTaxiState = Await.result(future, Duration(5, TimeUnit.SECONDS))
+          updateView(canvas, taxiWithLabel, responseTaxiState)
+          taxiWithLabel = responseTaxiState
           lastUpdate = now
         }
       }
       }
-
       timer.start()
     }
   }
 
+  private def initializeView(canvas: Group, config: SimulationConfig): Tuple2Zipped[Text, List[Text], Circle, List[Circle]] = {
+    val initialTaxi = config.taxis
+    val taxiLabels: List[Text] = initialTaxi.map(t => createText(t._1, Location(t._2.defaultNode.location.x, t._2.defaultNode.location.y))).toList
+    val taxiCircles: List[Circle] = initialTaxi.map(t => drawTaxi(Taxi(t._1, Location(t._2.defaultNode.location.x, t._2.defaultNode.location.y), TaxiState.Free))).toList
+
+    taxiCircles.foreach(tc => canvas.getChildren.add(tc))
+    taxiLabels.foreach(label => canvas.getChildren.add(label))
+
+    (taxiLabels, taxiCircles).zipped
+  }
+
+  private def updateValues(responseTaxiState: TaxiSystemState): Tuple2Zipped[Text, List[Text], Circle, List[Circle]] = {
+    val tempTaxis = responseTaxiState.taxis.map(t => Taxi(t.id, t.location, t.taxiState))
+    val taxiLabels: List[Text] = tempTaxis.map(t => createText(t.id, t.location))
+    val taxiCircles: List[Circle] = tempTaxis.map(t => drawTaxi(t))
+    val taxiWithLabel = (taxiLabels, taxiCircles).zipped
+    taxiWithLabel
+  }
+
+  private def updateView(canvas: Group, oldTaxiWithLabel: Tuple2Zipped[Text, List[Text], Circle, List[Circle]], taxiWithLabel: Tuple2Zipped[Text, List[Text], Circle, List[Circle]]) = {
+    oldTaxiWithLabel.foreach((label, taxi) => canvas.children.removeAll(label, taxi))
+    taxiWithLabel.foreach((label, taxi) => canvas.children.addAll(label, taxi))
+  }
+
   private def createLegend(canvas: Group) = {
-    canvas.getChildren.add(createText("free taxi", Location(boardWidth - 200, 0)))
-    canvas.getChildren.add(createCircle(boardWidth - 200, 25, freeStateColor))
-    canvas.getChildren.add(createText("on the way to customer taxi", Location(boardWidth - 200, 50)))
-    canvas.getChildren.add(createCircle(boardWidth - 200, 75, onWayToCustomerStateColor))
-    canvas.getChildren.add(createText("occupied taxi", Location(boardWidth - 200, 100)))
-    canvas.getChildren.add(createCircle(boardWidth - 200, 125, occupiedStateColor))
+    canvas.getChildren.add(createText("free taxi", Location(20, 0)))
+    canvas.getChildren.add(createCircle(20, 25, freeStateColor))
+    canvas.getChildren.add(createText("occupied taxi", Location(120, 0)))
+    canvas.getChildren.add(createCircle(120, 25, occupiedStateColor))
+    canvas.getChildren.add(createText("on the way to customer taxi", Location(240, 0)))
+    canvas.getChildren.add(createCircle(240, 25, onWayToCustomerStateColor))
+    canvas.getChildren.add(createText("order", Location(440, 0)))
+    canvas.getChildren.add(createSquare(440, 25, orderColor))
+
   }
 
   private def createText(name: String, location: Location): Text = {
     val text = new Text(name)
-    text.fill = Black
+    text.fill = textColor
     text.relocate(location.x + padding, location.y + padding)
     text.boundsType = TextBoundsType.Visual
     text
   }
 
   private def createStaticMap(canvas: Group): Unit = {
-    val circles: List[Circle] = config.cityMap.getCityMapElements.nodes.map(node => createCircle(node.location.x, node.location.y, Blue))
-    val paths: List[Path] = config.cityMap.getCityMapElements.edges.map(edge => generatePath(List(new Point2D(edge.first.location.x, edge.first.location.y),
-      new Point2D(edge.second.location.x, edge.second.location.y))))
+    val mapElements = config.cityMap.getCityMapElements
+    val circles: List[Circle] = mapElements.nodes.map(node => createCircle(node.location.x, node.location.y, mapColor))
+    val paths: List[Path] = mapElements.edges.map(edge => generatePath(List(new Point2D(edge.first.location.x, edge.first.location.y),
+      new Point2D(edge.second.location.x, edge.second.location.y)
+    )))
     circles.foreach(circle => canvas.getChildren.add(circle))
     paths.foreach(path => canvas.getChildren.add(path))
+  }
+
+  private def createSquare(x: Double, y: Double, color: Color): Rectangle = {
+    val square = new Rectangle()
+    square.fill = color
+    square.width = squareSize
+    square.height = squareSize
+    square.relocate(x - pointSize, y - pointSize)
+    square
   }
 
   private def createCircle(x: Double, y: Double, color: Color): Circle = {
@@ -133,7 +138,7 @@ class GUI(taxiSystem: TaxiSystem,
     path
   }
 
-  private def randomState(): TaxiState = {
+  def randomState(): TaxiState = {
     val number = Random.nextInt(3)
     number match {
       case 0 => TaxiState.Free
@@ -142,7 +147,7 @@ class GUI(taxiSystem: TaxiSystem,
     }
   }
 
-  private def getRandomLocation: Location = {
+  def getRandomLocation: Location = {
     Location(Random.nextInt(boardWidth - 50), Random.nextInt(boardHeight - 50))
   }
 
@@ -165,11 +170,17 @@ class GUI(taxiSystem: TaxiSystem,
 
 object GUI {
   val pointSize = 10
+  val squareSize = 2 * pointSize
   val boardWidth = 800
   val boardHeight = 800
   val refreshTime = 1000000000L
   val padding = 18
-  val freeStateColor = Green
+  var lastUpdate = 0L
+  val freeStateColor = YellowGreen
   val occupiedStateColor = Red
   val onWayToCustomerStateColor = DarkMagenta
+  val orderColor = Orange
+  val boardColor = WhiteSmoke
+  val textColor = Black
+  val mapColor = Blue
 }
